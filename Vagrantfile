07f3c23a1aaa58e6ca762b8345c612427a4c6138
abegorov <abegorov@github.com>
2024-07-05 04:21:56 +0300

    Vargantfile + provision.sh

---
+++ Vagrantfile
@@ -0,0 +1,25 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+MACHINES = {
+  :"kernel-update" => {
+    :box_name => "generic/centos9s",
+    :cpus => 16,
+    :memory => 8192,
+  }
+}
+
+Vagrant.configure("2") do |config|
+  MACHINES.each do |boxname, boxconfig|
+    config.vm.synced_folder ".", "/vagrant"
+    config.vm.define boxname do |box|
+      box.vm.box = boxconfig[:box_name]
+      box.vm.host_name = boxname.to_s
+      box.vm.provider "virtualbox" do |v|
+        v.memory = boxconfig[:memory]
+        v.cpus = boxconfig[:cpus]
+      end
+      box.vm.provision "shell", path: "provision.sh"
+    end
+  end
+end
---
+++ provision.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+set -e  # Exit immediately if a command exits with a non-zero status.
+set -u  # Treat unset variables as an error when substituting.
+set -x  # Print commands and their arguments as they are executed.
+
+LINUX_VER="6.10-rc6"
+VBOX_VER="7.0.18"
+
+echo Changing GRUB_TIMEOUT=1 to GRUB_TIMEOUT=10:
+sudo sed 's/^\(GRUB_TIMEOUT=\).*/\110/' -i /etc/default/grub
+sudo grub2-mkconfig -o /boot/grub2/grub.cfg
+sudo grub2-editenv - set menu_auto_hide=0
+
+echo Install latest kernel from elrepo:
+sudo dnf install -y \
+  "https://www.elrepo.org/elrepo-release-9.el9.elrepo.noarch.rpm"
+sudo dnf --enablerepo elrepo-kernel install -y kernel-ml kernel-ml-devel
+
+echo Install kernel build dependencies:
+sudo dnf install -y bc dwarves rpm-build
+
+# echo Create kernel build directory:
+# sudo mkdir "/usr/src/kernels/${LINUX_VER}"
+# sudo chown vagrant:vagrant "/usr/src/kernels/${LINUX_VER}"
+# cd "/usr/src/kernels/${LINUX_VER}"
+
+echo Download kernel sources:
+mkdir linux
+cd linux
+wget -O - "https://git.kernel.org/torvalds/t/linux-${LINUX_VER}.tar.gz" \
+  | tar xfz - --strip-components=1
+
+echo Build kernel RPM packages:
+make olddefconfig
+make -j 32
+make binrpm-pkg
+
+echo Install kernel RPM packages:
+sudo dnf install -y ./rpmbuild/RPMS/x86_64/*.rpm
+
+echo Update VirtualBox Guest Additions
+echo (installed version incompatible with new kernels):
+wget -O "/tmp/VBoxGuestAdditions.iso" \
+  "https://download.virtualbox.org/virtualbox/${VBOX_VER}/VBoxGuestAdditions_${VBOX_VER}.iso"
+sudo mount -o loop,ro "/tmp/VBoxGuestAdditions.iso" /mnt
+sudo /mnt/VBoxLinuxAdditions.run
+sudo umount /mnt
+rm "/tmp/VBoxGuestAdditions.iso"
+
+echo Build VirtualBox kernel modules for all kernels:
+sudo rcvboxadd quicksetup all
+
+sudo reboot
